aa37aef887afadb5c356397828e1e78aa505ad99
thiagoalmeida37 <thiago.o.almeida@hotmail.com>
2023-06-25 20:03:50 -0300

    first commit
    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return(
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+      <Text key="1" style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48,
+      }}>
+        Nome do evento
+      </Text >
+        
+      <Text key="2" style={{
+        color: '#6B6B6B',
+        fontSize: 16,
+      }}>
+        Sexta1 4 de Dezembro de 2023.
+      </Text>
+    </View>
+  ) 
+}
